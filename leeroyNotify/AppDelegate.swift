fee56912f2c74bc30476b28e1522b27103f9f808
Timo Zimmermann <timo@screamingatmyscreen.com>
2015-09-26 14:19:57 +0200

    Initial Commit

---
+++ leeroyNotify/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  leeroyNotify
+//
+//  Created by Timo Zimmermann on 26.09.15.
+//  Copyright © 2015 Timo Zimmermann. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
